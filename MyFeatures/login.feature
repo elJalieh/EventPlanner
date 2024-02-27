8a8a2b30306c59de39c4bfeb847ba132ef19f6fb
Ala'a Abdelrahim <alaraid2003@gmail.com>
2024-02-27 17:54:54 +0200

    new

--- MyFeatures/login.feature
+++ MyFeatures/login.feature
@@ -1,32 +1,46 @@
-Feature: User Login
+Feature:Login
 
-  Scenario Outline: User logs in successfully with valid credentials
-    Given the user accesses the sign-in command
-    When the user provides valid information '<username>' , '<password>', '<role>'
-    Then the user should be successfully logged in
-    Examples:
-      | username                | password  | role |
-      | Ayman                   | 132       | a    |
-      | s12127670@stu.najah.edu | 12345     | u    |
-      | s12127747@stu.najah.edu | 12345     | u    |
-      | asd2@gmail.com          | 1323      | s    |
-
-  Scenario: User login failed with valid credentials
-    Given the user accesses the login command
-    When the user provides valid information "Ayman" , "132" and something went wrong
-    Then the system should display an error message "<error_message>"
-    And the user should not be logged in
-
-
-  Scenario Outline: User login with invalid credentials
-    Given the user accesses the sign-in command
-    When the user provides information '<username>' , '<password>'
-    Then the system should display an error message "<error_message>"
-    And the user should not be logged in
+  Scenario Outline: Valid information
+    Given that the user is not logged in
+    When the information is valid email is "<Email>" and password is "<Password>"
+    And verification code is 12345
+    Then user successfully log in
 
     Examples:
-      | username       | password  | error_message       |
-      | invaliduser    | invalid   | Invalid username    |
-      | Ayman          | 111       | Invalid password    |
-      | non@gmail.com  | 111       | User does not exist |
-      |                |           | empty fields!       |
+      | Email                      | Password |
+      | a2y2m2a2n@gmail.com        | 123      |
+      | mo.matar123@gmail.com      | 1234     |
+      | alaraid2003@gmail.com      | 12345    |
+
+  Scenario Outline: Invalid email
+    Given that the user is not logged in
+    When the email is invalid email is "<Email>" and password is "<Password>"
+    Then user failed in log in
+
+    Examples:
+      | Email                       | Password |
+      | a2y2m2sdfgsda2n@gmail.com   | 123      |
+      | mo.matasdfgr123@gmail.com   | 1234     |
+      | alaraid20sdfg03@gmail.com   | 12345    |
+
+  Scenario Outline: Invalid password
+    Given that the user is not logged in
+    When the password is invalid email is "<Email>" and password is "<Password>"
+    Then user failed in log in
+
+    Examples:
+      | Email                       | Password |
+      | a2y2m2a2n@gmail.com         | 12311    |
+      | mo.matar123@gmail.com       | 123421   |
+      | alaraid2003@gmail.com       | 1234521  |
+
+
+  Scenario Outline: Invalid information
+    Given that the user is not logged in
+    When the information are invalid email is "<Email>" and password is "<Password>"
+    Then user failed in log in
+    Examples:
+      | Email                       | Password |
+      | a2y2msdf2a2n@gmail.com      | 12sdf3   |
+      | mo.matasadfr123@gmail.com   | 12sdf34  |
+      |                             |          |
--- src/test/java/special/planner/AcceptanceTest/login.java
+++ src/test/java/special/planner/AcceptanceTest/login.java
@@ -3,40 +3,47 @@ package special.planner.AcceptanceTest;
 import io.cucumber.java.en.*;
 
 public class login {
-    @Given("the user accesses the sign-in command")
-    public void theUserAccessesTheSignInCommand() {
+    @Given("that the user is not logged in")
+    public void thatTheUserIsNotLoggedIn() {
         // Write code here that turns the phrase above into concrete actions
+
     }
-    @When("the user provides valid information {string} , {string}, {string}")
-    public void theUserProvidesValidInformation(String string, String string2, String string3) {
+    @When("the information is valid email is {string} and password is {string}")
+    public void theInformationIsValidEmailIsAndPasswordIs(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
+
     }
-    @Then("the user should be successfully logged in")
-    public void theUserShouldBeSuccessfullyLoggedIn() {
+    @When("verification code is {int}")
+    public void verificationCodeIs(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user successfully log in")
+    public void userSuccessfullyLogIn() {
+        // Write code here that turns the phrase above into concrete actions
+
     }
 
-    //////////////////////////////////////////////////////////
-    @Given("the user accesses the login command")
-    public void theUserAccessesTheLoginCommand() {
+    @When("the email is invalid email is {string} and password is {string}")
+    public void theEmailIsInvalidEmailIsAndPasswordIs(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
+
     }
-    @When("the user provides valid information {string} , {string} and something went wrong")
-    public void theUserProvidesValidInformationAndSomethingWentWrong(String string, String string2) {
+    @Then("user failed in log in")
+    public void userFailedInLogIn() {
         // Write code here that turns the phrase above into concrete actions
-    }
-    @Then("the system should display an error message {string}")
-    public void theSystemShouldDisplayAnErrorMessage(String string) {
-        // Write code here that turns the phrase above into concrete actions
-    }
-    @Then("the user should not be logged in")
-    public void theUserShouldNotBeLoggedIn() {
-        // Write code here that turns the phrase above into concrete actions
+
     }
 
-///////////////////////////////
-    @When("the user provides information {string} , {string}")
-    public void theUserProvidesInformation(String string, String string2) {
+    @When("the password is invalid email is {string} and password is {string}")
+    public void thePasswordIsInvalidEmailIsAndPasswordIs(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("the information are invalid email is {string} and password is {string}")
+    public void theInformationAreInvalidEmailIsAndPasswordIs(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+
     }
 }
